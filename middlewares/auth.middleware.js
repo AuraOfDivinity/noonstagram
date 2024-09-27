8e5e2e69b91f5f0cd6ba43c09288b1b538517400
auraofdivinity <auraofdivinity@gmail.com>
2024-09-27 15:55:51 +0530

    Splitting jwt token before passing it forward.

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -5,16 +5,27 @@ const jwt = require("jsonwebtoken");
 // Middleware to verify JWT token
 exports.verifyToken = (req, res, next) => {
   const token = req.headers["authorization"];
+
   if (!token)
     return res
       .status(401)
       .json({ message: "Access denied. No token provided." });
 
+  // Extract the token from "Bearer <token>"
+  const tokenParts = token.split(" ");
+  if (tokenParts.length !== 2 || tokenParts[0] !== "Bearer") {
+    return res.status(400).json({ message: "Invalid token format." });
+  }
+
+  const jwtToken = tokenParts[1];
+
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log({ jwtToken }); // Log the extracted token
+    const decoded = jwt.verify(jwtToken, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (err) {
+    console.log({ err });
     res.status(400).json({ message: "Invalid token." });
   }
 };
