f290d0d26ffd31391c46c1101ee9558ca4b973c3
auraofdivinity <auraofdivinity@gmail.com>
2024-10-03 18:59:17 +0530

    Adding connection pool.

--- config/db.config.js
+++ config/db.config.js
@@ -1,16 +1,33 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log("Connected to MySQL database");
+db.getConnection((err, connection) => {
+  if (err) {
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      console.error("Database connection was closed.");
+    } else if (err.code === "ER_CON_COUNT_ERROR") {
+      console.error("Database has too many connections.");
+    } else if (err.code === "ECONNREFUSED") {
+      console.error("Database connection was refused.");
+    } else {
+      console.error("Database connection error:", err.message);
+    }
+  }
+
+  if (connection) {
+    console.log("Connected to MySQL database");
+    connection.release();
+  }
 });
 
 module.exports = db;
